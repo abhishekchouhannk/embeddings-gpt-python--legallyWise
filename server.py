67fd8f703d91a052b0b24c7079c18527b38edbdd
Abhishek <abhishekchouhannk@gmail.com>
2023-05-18 21:47:07 -0700

    adfhadsklfwdsf

--- server.py
+++ server.py
@@ -7,12 +7,6 @@ from http import HTTPStatus
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/hello':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b'Hello from custom route!')
-        else:
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         msg = 'Python is running on Qoddi! You requested %s' % (self.path)
